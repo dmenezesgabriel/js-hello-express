c87cc239de685bdace53cf2789ee458b43a37aad
Gabriel Menezes <dmenezes.gabriel@gmail.com>
2023-09-23 11:50:41 -0300

    add routes

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # Hello, Express
+
+```sh
+curl -X POST -H 'Content-Type: application/json' -d '{ "id": 4, "title": "A Game of Thrones", "author": "George R.R. Martin" }' http://localhost:3000/books
+```
--- src/app.js
+++ src/app.js
@@ -2,6 +2,8 @@ import express from "express";
 
 const app = express();
 
+app.use(express.json());
+
 const books = [
   { id: 1, title: "Harry Potter", author: "J.K. Rowling" },
   { id: 2, title: "Lord of the Rings", author: "J.R.R. Tolkien" },
@@ -12,8 +14,37 @@ app.get("/", (req, res) => {
   res.status(200).send("Hello, world!");
 });
 
+app.get("/books/:id", (req, res) => {
+  const index = searchBooksById(req.params.id);
+  res.status(200).json(books[index]);
+});
+
 app.get("/books", (req, res) => {
   res.status(200).json(books);
 });
+
+app.post("/books", (req, res) => {
+  books.push(req.body);
+  res.sendStatus(201).send("Book added successfully");
+});
+
+app.put("/books/:id", (req, res) => {
+  const { id } = req.params;
+  const index = searchBooksById(id);
+  books[index].title = req.body.title;
+  books[index].author = req.body.author;
+  res.json(books);
+});
+
+app.delete("/books/:id", (req, res) => {
+  const { id } = req.params;
+  const index = searchBooksById(id);
+  books.splice(index, 1);
+  res.sendStatus(204).send("Book deleted successfully");
+});
+
+function searchBooksById(id) {
+  return books.findIndex((book) => book.id == id);
+}
 
 export default app;
