de9fdb9ec6c5aa9c7dec748a70f7497ceb4ff28d
Gabriel Menezes <dmenezes.gabriel@gmail.com>
2023-09-24 19:52:15 -0300

    update books relationship to authors

--- src/controllers/booksController.js
+++ src/controllers/booksController.js
@@ -3,7 +3,7 @@ import books from "../models/Book.js";
 class BookController {
   static getAll = async (req, res) => {
     try {
-      const result = await books.find({});
+      const result = await books.find().populate("author").exec();
       res.status(200).json(result);
     } catch (err) {
       return res.status(500).json({ message: err.message });
